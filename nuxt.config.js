46f9d5290cdaecda353b3d9e613c8af69a851e4d
Genta Awaya <g-awaya@6q.design>
2019-07-14 05:27:14 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdn.blog.st-hatena.com/css/blog.css' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/main.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    },
+    postcss: {
+      preset: {
+        autoprefixer: {
+          grid: true
+        }
+      }
+    }
+  }
+}
