349d76dbb06ea036678e1fa038a2d32c8b9351f5
rokuzeudon <rokuzeudon@gmail.com>
2019-07-19 01:42:54 +0900

    Add breadcrumb

--- components/layouts/top-editarea.vue
+++ components/layouts/top-editarea.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="top-editarea">
-    <p>タイトル下</p>
+    <nav class="gnav">
+      <h2 class="gnav-title">Webデザイナー<a href="#">@rokuzeudon</a>のブログ</h2>
+      <ul class="gnav-items">
+        <li class="gnav-item"><a href="#">学習</a></li>
+        <li class="gnav-item"><a href="#">活動</a></li>
+        <li class="gnav-item"><a href="#">思考</a></li>
+        <li class="gnav-item"><a href="#">生活</a></li>
+      </ul>
+    </nav>
+    <ul class="subnav">
+      <li class="subnav-item"><a href="#">連載</a></li>
+      <li class="subnav-item"><a href="#">このブログについて</a></li>
+    </ul>
   </div>
 </template>
