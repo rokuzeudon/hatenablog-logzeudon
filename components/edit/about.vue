5e8390eba5aa04e7ddf84c395be45f3d7c39d39a
rokuzeudon <rokuzeudon@gmail.com>
2019-08-14 14:39:09 +0900

    update: aboutページの微調整

---
+++ components/edit/about.vue
@@ -0,0 +1,54 @@
+<template>
+<!--ここから-->
+
+<section class="about-box">
+  <h3>LOGzeudon</h3>
+  <p>当ブログは、管理人自身による情報発信の練習場であり、Webっぽいことの実験場でもあります。<br>表示崩れや拙い内容をご容赦ください。</p>
+  <div class="author-box">
+    <div class="author-left">
+      <div class="author-img">
+        <img src="//cdn-ak.f.st-hatena.com/images/fotolife/r/rokuzeudon/20181113/20181113094535.png" width="100" height="100" alt="rokuzeudon">
+      </div>
+      <dl>
+        <dt><a href="../about#aboutme"><span>ろくぜうどん</span></a></dt>
+        <dd>id:rokuzeudon</dd>
+      </dl>
+    </div>
+    <div class="author-right">
+      <p>名古屋で働いている、UI/UXについて考えるのが好きなWebデザイナーです。現在はインハウスで自社開発ツールのデザインに携わっています。</p>
+      <div class="author-btns">
+        <a href="https://twitter.com/rokuzeudon" class="author-btn"><img src="//data.rokuzeudon.com/blog/img/icn-twitter.svg" alt="Twitter"> フォロー</a>
+        <a href="http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe" onclick="window.open('http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe', '', 'width=500,height=400'); return false;" class="author-btn"><img src="//data.rokuzeudon.com/blog/img/icn-hatenablog.svg" alt="はてなブログ"> 読者登録</a>
+        <a class="feedly author-btn" href="//feedly.com/i/subscription/feed/https://blog.rokuzeudon.com/feed" target="_blank">Feedly登録</a>
+      </div>
+    </div>
+  </div>
+  <h4>記事方針</h4>
+  <ol>
+    <li><strong>実際に自分が体験したことや感じたことについて書く！</strong>…自分で確かめない情報や人の意見をそのまま記事にしない</li>
+    <li><strong>誰かの役にたつ</strong>…誰か一人にでも喜んでもらえるようなコンテンツにする</li>
+  </ol>
+</section>
+
+<section id="privacy" class="about-box">
+  <h3 >Privacy Policy</h3>
+  <h4>アフィリエイト宣伝プログラムについて</h4>
+  <p>次のプログラムに参加しています</p>
+  <ul>
+    <li>Amazonアソシエイト・プログラム</li>
+    <li>Apple Online Storeアフィリエイトプログラム</li>
+  </ul>
+  <h4>アクセス解析ツールについて</h4>
+  <p>
+    当ブログでは、アクセス解析ツールGoogle Analyticsを利用したトラフィックデータ収集のためにCookie（クッキー）を利用しています。 <br>
+    この情報は匿名で収集され個人を特定するものではありません。このトラフィックデータ収集は、ブラウザ設定でCookieを無効にすることで拒否できます。<br>
+    詳しくは<a href="//www.google.com/intl/ja/policies/privacy/partners/">こちら</a>をご確認ください。
+  </p>
+  <h4>免責事項</h4>
+  <p>
+    当ブログの記事内容は、筆者の主観が多く含まれます。紹介した商品のご購入や各種サービスの利用によって発生したいかなる結果にも一切責任を負いかねますので、予めご了承ください。
+  </p>
+</section>
+
+<!--ここまで-->
+</template>
