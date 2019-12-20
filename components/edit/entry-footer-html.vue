df49c6a41ac62e3cb94a81392b6fe4d2ee936b8d
rokuzeudon <g-awaya@6q.design>
2019-12-20 17:39:47 +0900

    style: この記事を書いた人の外観変更

--- components/edit/entry-footer-html.vue
+++ components/edit/entry-footer-html.vue
@@ -27,18 +27,20 @@
 <div class="author-box">
   <div class="author-left">
     <div class="author-img">
-      <img src="//cdn-ak.f.st-hatena.com/images/fotolife/r/rokuzeudon/20181113/20181113094535.png" width="100" height="100" alt="rokuzeudon" class="">
+      <img src="//cdn-ak.f.st-hatena.com/images/fotolife/r/rokuzeudon/20181113/20181113094535.png" alt="rokuzeudon" class="">
     </div>
-    <dl>
-      <dt><a href="../about#aboutme"><span>ろくぜうどん</span></a></dt>
-      <dd>id:rokuzeudon</dd>
-    </dl>
+    <div class="author-name">
+      <a href="../about#aboutme">ろくぜうどん</a>
+    </div>
+    <div class="author-id">id:rokuzeudon</div>
   </div>
   <div class="author-right">
-    <p>名古屋で働いている、UI/UXについて考えるのが好きなWebデザイナーです。現在はインハウスで自社開発ツールのデザインに携わっています。</p>
-    <div class="share-btns">
-      <a class="share-btn" href="https://twitter.com/rokuzeudon"><img src="//data.rokuzeudon.com/blog/img/icn-twitter.svg" alt="Twitter"> フォロー</a>
-      <a class="share-btn" href="http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe" onclick="window.open('http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe', '', 'width=500,height=400'); return false;"><img src="//data.rokuzeudon.com/blog/img/icn-hatenablog.svg" alt="はてなブログ"> 読者登録</a>
+    <div class="author-message">
+      <p>名古屋で働いている、UI/UXについて考えるのが好きなWebデザイナーです。現在はインハウスで自社開発ツールのデザインに携わっています。</p>
+      <div class="share-btns">
+        <a class="share-btn" href="https://twitter.com/rokuzeudon"><img src="//data.rokuzeudon.com/blog/img/icn-twitter.svg" alt="Twitter"> フォロー</a>
+        <a class="share-btn" href="http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe" onclick="window.open('http://blog.hatena.ne.jp/rokuzeudon/blog.rokuzeudon.com/subscribe', '', 'width=500,height=400'); return false;"><img src="//data.rokuzeudon.com/blog/img/icn-hatenablog.svg" alt="はてなブログ"> 読者登録</a>
+      </div>
     </div>
   </div>
 </div>
